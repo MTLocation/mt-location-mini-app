2724766c56daeed7feb330e257ed816b0c4df0bb
MTLocation <info@mtlocationremorques.ca>
2026-08-18 21:35:21 -0400

    Add RootLayout component with metadata for app

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "MT Location Remorques",
+  description: "Prise de possession et retour de remorque",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
