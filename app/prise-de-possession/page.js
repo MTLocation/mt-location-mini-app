129f4426282fab38c9becb060603d535f1fd544f
MTLocation <info@mtlocationremorques.ca>
2026-08-21 22:16:28 -0400

    Change button background color to black

--- app/prise-de-possession/page.js
+++ app/prise-de-possession/page.js
@@ -247,7 +247,7 @@ export default function PriseDePossession() {
               padding: "17px",
               border: "none",
               borderRadius: "12px",
-              background: "#d4AF37",
+              background: "#000000",
               color: "#ffffff",
               fontSize: "18px",
               fontWeight: "700",
