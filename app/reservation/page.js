fef74f99984a5666e93a7595032e06ef1c0b5733
MTLocation <info@mtlocationremorques.ca>
2026-08-22 15:19:08 -0400

    Add button to navigate to '/contrat' page

--- app/reservation/page.js
+++ app/reservation/page.js
@@ -167,6 +167,7 @@ overflow: "hidden",
 
           <button
             type="button"
+onClick={() => (window.location.href = "/contrat")}
             style={{
               width: "100%",
               padding: "17px",
