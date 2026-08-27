5b71c79ddd7534493d05215e73ebad84aa878f7f
MTLocation <info@mtlocationremorques.ca>
2026-08-26 22:29:15 -0400

    Ajout page attelage

---
+++ app/attelage/page.js
@@ -0,0 +1,37 @@
+"use client";
+
+export default function Attelage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#0b0b0b",
+        color: "#ffffff",
+        fontFamily: "Arial, sans-serif",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <div>
+        <img
+          src="/logo-mt.PNG"
+          alt="MT Location Remorques"
+          style={{
+            width: "240px",
+            maxWidth: "85%",
+            height: "auto",
+          }}
+        />
+
+        <h1>Comment atteler votre remorque</h1>
+
+        <p style={{ color: "#aaaaaa" }}>
+          La vidéo explicative sera disponible prochainement.
+        </p>
+      </div>
+    </main>
+  );
+}
