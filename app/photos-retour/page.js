9aea0836295d4379713d6831ba90c119c56cc873
MTLocation <info@mtlocationremorques.ca>
2026-08-28 15:01:45 -0400

    Import ProgressionEtapes component in PhotosRetour

--- app/photos-retour/page.js
+++ app/photos-retour/page.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import ProgressionEtapes from "../components/ProgressionEtapes";
 
 export default function PhotosRetour() {
   const router = useRouter();
