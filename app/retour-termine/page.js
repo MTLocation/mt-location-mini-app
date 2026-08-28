67931fe722913aed6977d2f97f36c1794d4f4454
MTLocation <info@mtlocationremorques.ca>
2026-08-28 14:41:59 -0400

    Create page.js

---
+++ app/retour-termine/page.js
@@ -0,0 +1,112 @@
+"use client";
+
+export default function RetourTermine() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        width: "100%",
+        background: "#0b0b0b",
+        color: "#ffffff",
+        fontFamily: "Arial, sans-serif",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "20px",
+        boxSizing: "border-box",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "430px",
+          margin: "0 auto",
+          textAlign: "center",
+        }}
+      >
+        <img
+          src="/logo-mt.PNG"
+          alt="MT Location Remorques"
+          style={{
+            width: "240px",
+            maxWidth: "85%",
+            height: "auto",
+            display: "block",
+            margin: "0 auto 20px",
+          }}
+        />
+
+        <div
+          style={{
+            width: "70px",
+            height: "70px",
+            borderRadius: "50%",
+            background: "#f2c94c",
+            color: "#111111",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: "34px",
+            fontWeight: "700",
+            margin: "0 auto 20px",
+          }}
+        >
+          ✓
+        </div>
+
+        <h1
+          style={{
+            fontSize: "30px",
+            margin: "0 0 10px",
+          }}
+        >
+          Retour terminé
+        </h1>
+
+        <p
+          style={{
+            color: "#cccccc",
+            fontSize: "17px",
+            lineHeight: "24px",
+            margin: "0 0 20px",
+          }}
+        >
+          Merci! Votre retour a bien été enregistré.
+        </p>
+
+        <div
+          style={{
+            background: "#151515",
+            border: "1px solid #444444",
+            borderRadius: "18px",
+            padding: "20px",
+            textAlign: "left",
+          }}
+        >
+          <div
+            style={{
+              fontSize: "16px",
+              lineHeight: "24px",
+            }}
+          >
+            Avant de quitter, assurez-vous que :
+          </div>
+
+          <div
+            style={{
+              marginTop: "12px",
+              fontSize: "16px",
+              lineHeight: "26px",
+            }}
+          >
+            ✓ La remorque est bien verrouillée
+            <br />
+            ✓ Tout l’équipement est remis en place
+            <br />
+            ✓ Vous n’avez rien oublié dans la remorque
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
