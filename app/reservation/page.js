b7997f3e5001883a87be3e6ae539873ce7d1c4b1
MTLocation <info@mtlocationremorques.ca>
2026-09-06 22:40:52 -0400

    Fix button onClick handler syntax

--- app/reservation/page.js
+++ app/reservation/page.js
@@ -216,7 +216,7 @@ function continuer() {
             type="button"
             onClick={continuer}
               
-            }
+            
             style={{
               width: "100%",
               padding: "17px",
